36d9e08117f38b20e754ffbfc2daef3b04573954
tsuki <lucasmoon0@gmail.com>
2023-05-07 11:21:55 +0200

    fix print_nordvpn_status

--- scripts/status.sh
+++ scripts/status.sh
@@ -8,14 +8,14 @@ connecting_text=$(get_tmux_option "@nordvpn_connecting_text" "Connecting")
 disconnected_text=$(get_tmux_option "@nordvpn_disconnected_text" "Disconnected")
 
 print_nordvpn_status() {
-    status=$(nordvpn status | grep 'Status:')
+    status=$(nordvpn status | grep 'Status:\s.*')
 
-    if [[ $status == "Disconnected" ]]; then
-        echo "$disconnected_text"
-    elif [[ $status == "Connecting" ]]; then
-        echo "$connecting_text"
-    elif [[ $status == "Connected" ]]; then
-        echo "$connected_text"
+    if [[ "$status" == *"Disconnected"* ]]; then
+      echo "$disconnected_text"
+    elif [[ "$status" == *"Connecting"* ]]; then
+      echo "$connecting_text"
+    elif [[ "$status" == *"Connected"* ]]; then
+      echo "$connected_text"
     fi
 }
 
